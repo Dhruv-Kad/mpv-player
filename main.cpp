f7460995bc970d671c7259517ed0f01541ade40e
Dhruv <dhruv@gitignore.com>
2025-06-02 02:58:17 -0500

    Realized that there is a better way to add valid file types that factors for unusual file names

--- main.cpp
+++ main.cpp
@@ -3,52 +3,42 @@
 #include <iostream>
 #include <string>
 #include <vector>
-int main() {
+
+const std::string valid_types[58] = {
+    ".mkv",  ".mk3d", ".mka", ".mks",  ".mp4",  ".m4v",  ".mov",
+    ".avi",  ".webm", ".ogm", ".mpg",  ".mpeg", ".ts",   ".m2ts",
+    ".rm",   ".rmvb", ".wmv", ".asf",  ".flv",  ".f4v",  ".3gp",
+    ".nut",  ".bik",  ".dv",  ".bdmv", ".mpls",
+
+    ".mp3",  ".aac",  ".m4a", ".flac", ".wav",  ".opus", ".ogg",
+    ".alac", ".dts",  ".ac3", ".wma",  ".amr",  ".spx",  ".mpc",
+    ".ape",  ".ra",   ".shn", ".imc",
+
+    ".srt",  ".ass",  ".ssa", ".sub",  ".rt",   ".smi",  ".idx",
+    ".pjs",  ".jss",  ".aqt", ".vtt",  ".ttml", ".dvb",  ".ttxt",
+};
+void sanitized_select() {
   std::string line;
-  bool collect = false;
   std::string extension;
   int number = 0;
-  std::string valid_types[59] = {
-      ".mkv",  ".mk3d", ".mka", ".mks",  ".mp4",  ".m4v",  ".mov",
-      ".avi",  ".webm", ".ogm", ".mpg",  ".mpeg", ".ts",   ".m2ts",
-      ".rm",   ".rmvb", ".wmv", ".asf",  ".flv",  ".f4v",  ".3gp",
-      ".nut",  ".bik",  ".dv",  ".bdmv", ".mpls",
-
-      ".mp3",  ".aac",  ".m4a", ".flac", ".wav",  ".opus", ".ogg",
-      ".alac", ".dts",  ".ac3", ".wma",  ".amr",  ".spx",  ".mpc",
-      ".ape",  ".ra",   ".shn", ".imc",
-
-      ".srt",  ".ass",  ".ssa", ".sub",  ".rt",   ".smi",  ".idx",
-      ".pjs",  ".jss",  ".aqt", ".vtt",  ".ttml", ".dvb",  ".ttxt",
-  };
-
+  bool collect = false;
   std::vector<std::string> avalfiles;
   // Get all files in the thing
-  std::system("ls -h >test.txt");
-  std::ifstream streamer("test.txt");
+  // Honestly, we don't even need to do this, we can just make it so only valids
+  // get ls'd
+  for (std::string m : valid_types) {
+    extension = "ls *";
+    extension += m;
+    extension += " >> validFilesList.txt 2>/dev/null";
+    std::system(extension.c_str());
+  }
+  std::ifstream streamer("validFilesList.txt");
   // Find compatiable files and add to list
   while (std::getline(streamer, line)) {
-    // Gets extension of file
-    collect = false;
-    extension = "";
-    for (char m : line) {
-      if (m == '.') {
-        collect = true;
-      }
-      if (collect) {
-        extension += m;
-      }
-    }
-    for (std::string m : valid_types) {
-      if (m == extension) {
-        number++;
-        std::cout << "[" << number << "]. " << line << "\n";
-        avalfiles.push_back(line);
-        break;
-      }
-    }
+    number++;
+    std::cout << "[" << number << "]. " << line << "\n";
+    avalfiles.push_back(line);
   }
-  system("rm test.txt");
   streamer.close();
   std::cout << "Enter a choice to play (0 to quit): ";
   std::cin >> number;
@@ -58,4 +48,9 @@ int main() {
     extension = "mpv \'" + line + "\'";
     std::system(extension.c_str());
   }
+  system("rm validFilesList.txt");
+}
+int main() {
+  sanitized_select();
+  // other options tba
 }
