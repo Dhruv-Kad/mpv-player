8bc88744835a40f9f36ba55bb4681b11ab2504e4
Dhruv <dhruv@gitignore.com>
2025-06-02 03:24:10 -0500

    more installer work

--- installer.sh
+++ installer.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-g++ main.cpp -o mpa
+if g++ main.cpp -o mpa; then
+  echo "Moving executable to /usr/local/bin/"
+else
+  if clangd main.cpp -o mpa; then
+    echo "Moving executable to /usr/local/bin/"
+  else
+    echo "Install either the GNU complier collection (g++) or clangd"
+    exit 1
+  fi
+fi 
+
+if sudo mv mpa /usr/local/bin/mpa; then
+  echo "mpa has been sucessfully installed!"
+  exit 0
+else
+  echo "Something went wrong."
+fi
 
